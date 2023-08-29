c5de42a79563ae83dc4a474d05a38d2e6e33d10f
Alebus7 <alexandre.dubus@epitech.eu>
2023-08-29 12:06:12 +0200

    Refactoring.

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -15,12 +15,6 @@
 
 <script setup>
 
-/**
- * Composant "Input" :
- * Ce composant représente une balise HTML <input> qui peut être de type "text" ou "button".
- * Pour être utilisé, ce composant doit obligatoirement recevoir l'attribut "type", qui ne peut avoir comme valeur que "text" ou "button".
- */
-
 // Définition des props :
 const props = defineProps({
 
@@ -39,22 +33,22 @@ const props = defineProps({
 
 <style scoped>
 
-/* Styles dans tous les cas : */
+/* Styles de l'input dans tous les cas : */
 input {
   border: none;
   border-radius: var(--small-border-radius);
   padding: var(--small-padding);
 }
 
-/* Styles dans le cas d'un champ texte : */
-.text-input {
+/* Styles de l'input dans le cas d'un champ texte : */
+input[type='text'] {
   background-color: var(--white-color);
   color: var(--dark-blue-color);
   box-shadow: var(--small-inner-shadow);
 }
 
-/* Styles dans le cas d'un bouton : */
-.button-input {
+/* Styles de l'input dans le cas d'un bouton : */
+input[type='button'] {
   background-color: var(--dark-blue-color);
   color: var(--white-color);
   box-shadow: var(--small-shadow);
